cb6ee8a09e72b23d6906023aa7123e1583226fb3
Franklyn R <franklyn1998@live.com>
2022-07-27 16:54:26 -0300

    Finalizando

--- app/src/main/java/com/hadesfranklyn/lcoolougasolina/MainActivity.java
+++ app/src/main/java/com/hadesfranklyn/lcoolougasolina/MainActivity.java
@@ -1,14 +1,72 @@
 package com.hadesfranklyn.lcoolougasolina;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputEditText editPrecoAlcool, editPrecoGasolina;
+    private TextView textoResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editPrecoAlcool = findViewById(R.id.editPrecoAlcool);
+        editPrecoGasolina = findViewById(R.id.editPrecoGasolina);
+        textoResultado = findViewById(R.id.textoResultado);
     }
+
+    public void calcularPreco(View view) {
+
+        //recuperar valores digitados
+        String precoAlcool = editPrecoAlcool.getText().toString();
+        String precoGasolina = editPrecoGasolina.getText().toString();
+
+        //validar os campos digitados
+        Boolean camposValidados = validarCampos(precoAlcool, precoGasolina);
+
+        if (camposValidados) {
+            calcular(precoAlcool, precoGasolina);
+        } else {
+            textoResultado.setText("Preencha os preços primeiro!");
+        }
+    }
+
+    public Boolean validarCampos(String pAlcool, String pGasolina) {
+
+        Boolean camposValidados = true;
+
+        if (pAlcool == null || pAlcool.equals("")) {
+            camposValidados = false;
+        } else if (pGasolina == null || pGasolina.equals("")) {
+            camposValidados = false;
+        }
+        return camposValidados;
+    }
+
+    public void calcular(String precoAlcool, String precoGasolina) {
+        //Convertendo string para numeros
+        Double valorAlcool = Double.parseDouble(precoAlcool);
+        Double valorGasolina = Double.parseDouble(precoGasolina);
+
+        /**
+         * Fazer calculo de menor preco
+         * Se (valorAlcool / valorGasolina) >= 0.7 é melhor utilizar gasolina
+         * Senao e melhor utilizar alcool
+         */
+        Double resultado = valorAlcool / valorGasolina;
+        if (resultado >= 0.7) {
+            textoResultado.setText("Melhor utilizar Gasolina");
+        } else {
+             textoResultado.setText("Melhor utilizar Álcool");
+        }
+    }
+
 }
